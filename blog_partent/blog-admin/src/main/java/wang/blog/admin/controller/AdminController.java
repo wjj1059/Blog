623383ee06ab3822f6b4186da0d01f78d55dbfda
wangjj <wangjj@vims.com.cn>
2022-02-22 17:20:05 +0800

    添加Controller层

---
+++ blog_partent/blog-admin/src/main/java/wang/blog/admin/controller/AdminController.java
@@ -0,0 +1,36 @@
+package wang.blog.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import wang.blog.admin.model.params.PageParam;
+import wang.blog.admin.pojo.Permission;
+import wang.blog.admin.service.Impl.PermissionServiceImpl;
+import wang.blog.admin.service.PermissionService;
+import wang.blog.admin.vo.Result;
+
+/**
+ * @author 王家俊
+ */
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+    @Autowired
+    private PermissionServiceImpl permissionService;
+
+    @PostMapping("permission/permissionList")
+    public Result listPermission(@RequestBody PageParam pageParam){
+      return permissionService.listPermission(pageParam);
+    }
+    @PostMapping("permission/add")
+    public Result add(@RequestBody Permission permission){
+        return permissionService.add(permission);
+    }
+    @PostMapping("permission/update")
+    public Result update(@RequestBody Permission permission){
+        return permissionService.update(permission);
+    }
+    @PostMapping("permission/delete/{id}")
+    public Result delete(@PathVariable("id") Long id){
+        return permissionService.delete(id);
+    }
+}
